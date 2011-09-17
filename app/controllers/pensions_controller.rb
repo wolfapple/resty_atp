fa173a28fbae481706432925cfae7d227180ac00
wolfapple <illumine03@naver.com>
2011-09-17 19:51:44 +0900

    markup start

--- app/controllers/pensions_controller.rb
+++ app/controllers/pensions_controller.rb
@@ -4,6 +4,7 @@ class PensionsController < ApplicationController
     @pensions = Pension.page(params[:page]).per(5)
     @pensions = @pensions.by_addr1(params[:addr1]) if params[:addr1]
     @pensions = @pensions.by_addr2(params[:addr2]) if params[:addr2]
+    @theme = Theme.find(params[:theme_id]) if params[:theme_id]
   end
   
   def show
