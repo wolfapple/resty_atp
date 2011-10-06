a3076d18f3e0a761a051a846132512abe2d7a0be
wolfapple <illumine03@naver.com>
2011-10-06 12:45:52 +0900

    area_spot delete

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -3,7 +3,6 @@ class Spot < ActiveRecord::Base
   has_many :spot_pensions
   has_many :pensions, :through => :spot_pensions
   has_many :reviews, :class_name => "SpotReview"
-  has_one :area_spot
-  has_one :area, :through => :area_spot
-  has_one :sub_area, :through => :area_spot
+  belongs_to :area
+  belongs_to :sub_area
 end
