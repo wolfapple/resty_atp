45e9a9f6e32a438dfa06ea382ea78f8c87bbcff6
wolfapple <illumine03@naver.com>
2011-10-07 19:24:23 +0900

    validate_uniqueness

--- app/models/spot_pension.rb
+++ app/models/spot_pension.rb
@@ -4,5 +4,5 @@ class SpotPension < ActiveRecord::Base
   belongs_to :pension
   
   validates_presence_of :spot_id, :pension_id
-  validates_uniqueness_of :spot_id, :pension_id
+  validates_uniqueness_of :spot_id, :scope => [:pension_id]
 end
--- app/models/theme_pension.rb
+++ app/models/theme_pension.rb
@@ -4,5 +4,5 @@ class ThemePension < ActiveRecord::Base
   belongs_to :pension
   
   validates_presence_of :theme_id, :pension_id
-  validates_uniqueness_of :theme_id, :pension_id
+  validates_uniqueness_of :theme_id, :scope => [:pension_id]
 end
