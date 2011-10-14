8bac6b886c00073f9ae8cc6f1dc74eaa64021850
wolfapple <illumine03@naver.com>
2011-10-14 12:35:44 +0900

    spot order

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -18,6 +18,6 @@ class Spot < ActiveRecord::Base
   def near_by_pensions(limit=7)
     #pensions = self.pensions.where("addr like '#{self.addr.split(' ')[2]}'").limit(limit)
     #pensions = pensions + self.pensions.limit(limit-pensions.count) if pensions.count < limit
-    Pension.near([self.latitude, self.longitude], 10, {:units => :km, :order => :distance, :limit => 15})
+    Pension.unscoped.near([self.latitude, self.longitude], 10, {:units => :km, :order => :distance, :limit => 15})
   end
 end
