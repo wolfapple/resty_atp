272378e0c36949873bcc65c8bca86e6aecf2d62e
wolfapple <illumine03@naver.com>
2011-10-20 14:53:23 +0900

    search update

---
+++ db/migrate/20111020031801_create_search_logs.rb
@@ -0,0 +1,10 @@
+class CreateSearchLogs < ActiveRecord::Migration
+  def change
+    create_table :search_logs do |t|
+      t.string :input
+      t.integer :count, :default => 0
+
+      t.timestamps
+    end
+  end
+end
