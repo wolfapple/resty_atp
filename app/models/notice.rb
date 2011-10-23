193947148f10ab5ec6c0bf830a79a22ef05f41b1
wolfapple <illumine03@naver.com>
2011-10-23 18:03:22 +0900

    weekend update

---
+++ app/models/notice.rb
@@ -0,0 +1,19 @@
+# -*- encoding : utf-8 -*-
+class Notice
+  attr_reader :title, :link
+  
+  def initialize(title, link)
+    @title = title
+    @link = link
+  end
+  
+  def self.recent
+    url = "http://restycorp.tistory.com/rss/xml"
+    require 'open-uri'
+    doc = Nokogiri::XML(open(url))
+    @results = doc.xpath('//item').map do |i|
+      Notice.new(i.xpath('title').inner_text, i.xpath('link').inner_text)
+    end
+    @results = @results[0...3]
+  end
+end
