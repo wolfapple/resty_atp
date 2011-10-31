9f101c93db8f42ebbfe4cb6e0a445c3436a39a50
wolfapple <illumine03@naver.com>
2011-10-31 14:39:53 +0900

    notice cache

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -8,9 +8,17 @@ class Notice
   end
   
   def self.recent
-    url = "http://resty.tistory.com/rss/xml"
-    require 'open-uri'
-    doc = Nokogiri::XML(open(url))
+    begin
+      cache = "#{Rails.root}/tmp/cache/notice.xml"
+      doc = Nokogiri::XML(open(cache))
+    rescue
+      url = "http://resty.tistory.com/rss/xml"
+      require 'open-uri'
+      doc = Nokogiri::XML(open(url))
+      cache = File.new("#{Rails.root}/tmp/cache/notice.xml", "w")
+      cache.write(doc.to_xml)
+      cache.close
+    end
     @results = doc.xpath('//item').map do |i|
       Notice.new(i.xpath('title').inner_text, i.xpath('link').inner_text)
     end
