0e01698f423609fe2fda687132009e9c281693ce
wolfapple <illumine03@naver.com>
2011-12-14 13:57:12 +0900

    mail

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,18 +1,10 @@
 # -*- encoding : utf-8 -*-
-# ActionMailer::Base.smtp_settings = {
-#   :address              => "smtp.gmail.com",
-#   :port                 => 587,
-#   :domain               => 'resty.co.kr',
-#   :user_name            => 'help@resty.co.kr',
-#   :password             => 'resty0808',
-#   :authentication       => 'plain',
-#   :enable_starttls_auto => true
-# }
 ActionMailer::Base.smtp_settings = {
-  :address              => "db.resty.co.kr",
+  :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'resty.co.kr',
-  :user_name            => 'wolfapple',
-  :password             => 'vldvos03',
-  :authentication       => 'plain'
+  :user_name            => 'help@resty.co.kr',
+  :password             => 'resty0808',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
 }
